6662cc9362a2fd2ada062780970ca7fa07aa820d
stot <rublevskyio@tl-kpi.kiev.ua>
2023-09-25 21:25:01 +0300

    Add project files.

---
+++ DS/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.skglControl1 = new SkiaSharp.Views.Desktop.SKGLControl();
+            this.Do1Iteration = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // skglControl1
+            // 
+            this.skglControl1.BackColor = System.Drawing.Color.Black;
+            this.skglControl1.Location = new System.Drawing.Point(12, 12);
+            this.skglControl1.Name = "skglControl1";
+            this.skglControl1.Size = new System.Drawing.Size(512, 512);
+            this.skglControl1.TabIndex = 0;
+            this.skglControl1.VSync = false;
+            this.skglControl1.PaintSurface += new System.EventHandler<SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs>(this.display);
+            // 
+            // Do1Iteration
+            // 
+            this.Do1Iteration.Location = new System.Drawing.Point(530, 12);
+            this.Do1Iteration.Name = "Do1Iteration";
+            this.Do1Iteration.Size = new System.Drawing.Size(150, 149);
+            this.Do1Iteration.TabIndex = 1;
+            this.Do1Iteration.Text = "Do1Iteration";
+            this.Do1Iteration.UseVisualStyleBackColor = true;
+            this.Do1Iteration.Click += new System.EventHandler(this.Do1Iteration_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 535);
+            this.Controls.Add(this.Do1Iteration);
+            this.Controls.Add(this.skglControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SkiaSharp.Views.Desktop.SKGLControl skglControl1;
+        private System.Windows.Forms.Button Do1Iteration;
+    }
+}
+
---
+++ DS/Form1.cs
@@ -0,0 +1,40 @@
+﻿using OpenTK.Graphics.ES20;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DS
+{
+    public partial class Form1 : Form
+    {
+        public int iterationNumber = 0;
+        public Random random = new Random(0);
+        public List<int[]> cubes = new List<int[]>();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void display(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
+        {
+            e.Surface.Canvas.Clear();
+            e.Surface.Canvas.DrawRect(0, 0, 100, 100, new SKPaint
+            {
+                Color = new SKColor((byte)random.Next(255), (byte)random.Next(255), (byte)random.Next(255))
+            }) ;
+        }
+
+        private void Do1Iteration_Click(object sender, EventArgs e)
+        {
+            skglControl1.Invalidate();
+        }
+    }
+}
