a9eb1ecf3c09b255269e10b57c870d513a163d81
stot <rublevskyio@tl-kpi.kiev.ua>
2023-10-09 19:57:53 +0300

    Ordered selection for a 2d midpoint

--- DS/Form1.cs
+++ DS/Form1.cs
@@ -1,7 +1,7 @@
 ﻿//Rublevsky O O
 /*
  *Eventually this will become a compleate Dimond-Square algoruthm implementation
- *but this version is only implementing midpoint generation
+ *but this version is only implementing midpoint generation , now in 2d
 */
 
 using OpenTK.Graphics.ES20;
@@ -24,10 +24,12 @@ namespace DS
     public partial class Form1 : Form
     {
         private static int DIMENSION = 1024;
+        private static int worldsize_param =10;
+        private static int WORLDSIZE = (int)Math.Pow(2,worldsize_param);
         private float ROUGHNESS = 0.5f;
         public int iterationNumber = 0;
         public static Random random = new Random();
-        public Node[524289][524289];
+        public Node[,] World;
         public List<Node> midpointLine = new List<Node>() { new Node(0, 0, 512.0f), new Node(DIMENSION, 0, 512.0f) };
         public Form1()
         {
@@ -36,14 +38,64 @@ namespace DS
 
         private void display(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
         {
-            e.Surface.Canvas.Clear();
-            e.Surface.Canvas.DrawRect(DIMENSION, 0, 3, DIMENSION, new SKPaint
-            {
-                Color = new SKColor(255, 0, 0)
-            });
-            subdivide(sender,e);
+            //e.Surface.Canvas.Clear();
+            //e.Surface.Canvas.DrawRect(DIMENSION, 0, 3, DIMENSION, new SKPaint
+            //{
+            //    Color = new SKColor(255, 0, 0)
+            //});
+            InitWorld();
+            generateLandscape(sender,e);
+            //subdivide(sender,e);
         }
-        private void subdivide(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
+        //Sets up the initial 4 points for the world corners
+        private void InitWorld()
+        {
+            World = new Node[WORLDSIZE+1, WORLDSIZE+1];
+            World[0, 0] = new Node(0, 0, 512.0f);
+            World[0, WORLDSIZE] = new Node(0, WORLDSIZE, 512.0f);
+            World[WORLDSIZE, 0] = new Node(WORLDSIZE, 0, 512.0f);
+            World[WORLDSIZE, WORLDSIZE] = new Node(WORLDSIZE, WORLDSIZE, 512.0f);
+        }
+        //the main function
+        private void generateLandscape(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
+        {
+            for (int i = 1; i <= worldsize_param; i++)
+            {
+                int step = WORLDSIZE;
+                step = step / (int)Math.Pow(2, i);
+                int numberOfSteps = WORLDSIZE/ step;
+                byte color = (byte)(random.NextDouble() * 255);
+                for (int y = 0; y < numberOfSteps+1; y++)
+                {
+                    for (int x = 0; x < numberOfSteps+1; x++) 
+                    {   
+                        if ((x+y)%2 != 0)
+                        {
+                            //e.Surface.Canvas.DrawRect(x*step, y*step,5,5, new SKPaint { Color = new SKColor(color, color, color) });
+                            e.Surface.Canvas.DrawPoint 
+                                (new SKPoint(x*step,y*step),
+                                new SKColor(color, 0, 0));
+                        }
+                    }
+                }
+                for (int y = 0; y < numberOfSteps + 1; y++)
+                {
+                    for (int x = 0; x < numberOfSteps + 1; x++)
+                    {
+                        if ((x + y) % 2 == 0)
+                        {
+                            //e.Surface.Canvas.DrawRect(x*step, y*step,5,5, new SKPaint { Color = new SKColor(color, color, color) });
+                            e.Surface.Canvas.DrawPoint
+                                (new SKPoint(x * step, y * step),
+                                new SKColor(0, color, 0));
+                        }
+                    }
+                }
+
+            }
+        }
+        //Step 1 : generate wall middles
+        /*private void subdivide(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
         {
             int segmentLen = DIMENSION / midpointLine.Count;
             List<Node> midpoints = new List<Node>();
@@ -56,7 +108,6 @@ namespace DS
                     (float)((midpointLine[i].height + midpointLine[i + 1].height) / 2 + ROUGHNESS * segmentLen * (random.NextDouble() - 0.5)));//new z
                 midpoints.Add(midpoint);
             }
-            //Debug.WriteLine(midpoints.Count);
             for (int i = 0; i<midpoints.Count;i++)
             {
                 midpointLine.Insert(2*i+1,midpoints[i]);
@@ -72,7 +123,7 @@ namespace DS
                     Color = new SKColor(color, color, color)
                 });
             }
-        }
+        }*/
     private void Do1Iteration_Click(object sender, EventArgs e)
         {
             skglControl1.Invalidate();
